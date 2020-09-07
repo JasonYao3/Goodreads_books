990653a6da4ae966f5e859e346e75fb92a435321
Jason Yao <50341059+JasonYao3@users.noreply.github.com>
2020-09-07 10:26:17 -0400

    Delete Load data.sql

--- Load data.sql
+++
@@ -1,9 +0,0 @@
-LOAD DATA INFILE  
-'H:/kaggle data/goodreadsbooks/Book1.csv'
-INTO TABLE kaggle_data  
-CHARACTER SET utf8mb4
-FIELDS ENCLOSED BY '"'
-TERMINATED BY ',' 
-LINES TERMINATED BY '\r\n'
-IGNORE 1 LINES
-(book_id, title, authors, average_rating, isbn, isbn13, language_code, num_pages, ratings_count, comments_count, publication_date, publisher);
